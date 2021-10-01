6d878ba289007fa487cf97f226f6ff670b48afab
valentina cantarelli <valentinacanterina@gmail.com>
2021-10-01 17:38:16 +0200

    cretata struttura album

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,19 +1,49 @@
 <template>
-  <div>
-    <Cover/>
-  </div>
+    <div class="album-container">
+        <div class="row" v-for="(elm,index) in album" :key="index">
+            <Cover :copertina="elm"/>
+        </div>
+    </div>
 </template>
 
 <script>
-import Cover from './Cover.vue'
+import Cover from './Cover.vue';
+import axios from 'axios';
+
 export default {
     name:"Albums",
     components:{
         Cover
-    }
+    },
+    data(){
+        return{
+            album:[]
+        }
+    },
+    created() {
+		axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+			.then( (res) => {
+				this.album = res.data.response;
+			});
+	}
 }
 </script>
 
 <style lang="scss" scope>
+.album-container{
+    display:flex;
+    width: 70%;
+    margin: 0 auto;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top:50px;
 
+    .row{
+        display: flex;
+        justify-content: center;
+        padding:20px;
+        text-align: center;
+    }
+    
+}
 </style>
--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,13 +1,38 @@
 <template>
-  <div></div>
+  <div>
+      <div class="card">
+          <img :src="copertina.poster" :alt="copertina.title">
+          <h3>{{copertina.title}}</h3>
+          <div>{{copertina.author}}</div>
+          <div>{{copertina.year}}</div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Cover"
+    name:"Cover",
+    props:["copertina"]
 }
 </script>
 
 <style lang="scss" scope>
-
+@import '../assets/style/variables';
+.card{
+    background-color:$secondaryColor ;
+    height: 230px;
+    width:150px;
+    
+    img{
+        height: 110px;
+        width: 110px;
+        margin-top:15px;
+    }
+    h3{
+        color:white;
+        text-transform: uppercase;
+        font-size: 15px;
+        padding:10px;
+    }
+}
 </style>
