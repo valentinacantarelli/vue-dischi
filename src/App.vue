571edc41eec4b54788717ad9689df3ac7281e75a
valentina cantarelli <valentinacanterina@gmail.com>
2021-10-01 15:32:27 +0200

    create regole common

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,16 @@
 import Header from './components/Header.vue'
 import Albums from './components/Albums.vue'
 
-
 export default {
   name: 'App',
   components: {
     Header,
     Albums
-
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style/common";
+
 </style>
